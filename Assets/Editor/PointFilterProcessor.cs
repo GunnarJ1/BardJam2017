6d44a1ca8dccb1a3103cdf4dac84fc0b7a0ace5f
Gunnar Jessee <gunnarjessee@gmail.com>
2017-02-25 13:14:23 -0500

    Added todo list\nAdded point filter processor

---
+++ Assets/Editor/PointFilterProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PointFilterProcessor : AssetPostprocessor
+{
+
+    public void OnPostprocessTexture(Texture2D texture)
+    {
+        //If is in Sprites director it will change the texture into point filter
+        if (assetPath.Contains("Sprites"))
+        {
+            TextureImporter textureImporter = (TextureImporter)assetImporter;
+            textureImporter.filterMode = FilterMode.Point;
+        }
+    }
+    
+}
